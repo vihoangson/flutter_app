50b6924cb8e01c8cb366370b6d629b7551ed90f1
lampart_vi_hoang_son <vihoangson@gmail.com>
2021-01-10 12:07:32 +0700

    fist lesssion

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,40 @@ import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:http/http.dart'  as http;
+import 'dart:math';
 // The main function is the starting point for all our flutter App.
 void main() {
-  runApp(MyApp());
+  runApp(AppSample());
+}
+class AppSample extends StatefulWidget {
+  @override
+  _AppSampleState createState() => _AppSampleState();
 }
 
-class MyApp extends StatelessWidget {
+class _AppSampleState extends State<AppSample> {
+  int string = 1;
+
+  void playSound(){
+    final player = AudioCache();
+    player.play('node1.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
           child: FlatButton(
             onPressed: ()async {
+              setState(() {
+                string = Random().nextInt(1000);
+                print('string: $string');
+              });
               print('ok men');
               final AudioCache player = new AudioCache();
-              player.play('audio/cello.mp3');
+              // player.play('audio/cello.mp3');
               var url = 'http://ttht.oop.vn/';
               var response = await  http.get(url);
               print(response);
@@ -30,7 +47,7 @@ class MyApp extends StatelessWidget {
             },
             height: 100.0,
             color: Colors.white,
-            child: Text('cello'),
+            child: Text('ss $string'),
           ),
         ),
       ),
