0e577acae4ddb198fb905b282e33043060d64788
lampart_vi_hoang_son <vihoangson@gmail.com>
2021-01-10 16:24:56 +0700

    Lesson 90: build score

--- lib/main.dart
+++ lib/main.dart
@@ -30,18 +30,62 @@ class _QuizzlerState extends State<Quizzler> {
     Icon(Icons.check,color:Colors.cyan),
     Icon(Icons.close,color:Colors.red),
     Icon(Icons.close,color:Colors.red),
+  ];
+
+  List<String> listQuestions =[
+    'Question 1',
+    'Question 2',
+    'Question 3',
+    'Question 4',
+    'Question 5',
+    'Question 6',
+    'Question 7',
+    'Question 8',
+    'Question 9',
 
   ];
+
+  int currentQuesion = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          backgroundColor: Colors.green,
+          backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('Ok');
+                },
+                color: Colors.black,
+                child: Text(listQuestions[currentQuesion],
+                  style: TextStyle(
+                    color:Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  print('Ok');
+
+                  setState(() {
+                    if(currentQuesion >= listQuestions.length-1)
+                      currentQuesion = -1;
+                    currentQuesion++;
+                    print(currentQuesion);
+                  });
+
+                },
+                color: Colors.green,
+                child: Text('Ok'),
+              ),
+            ),
             Expanded(
               child: FlatButton(
                 onPressed: () {
@@ -52,25 +96,8 @@ class _QuizzlerState extends State<Quizzler> {
               ),
             ),
             Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  print('Ok');
-                },
-                color: Colors.green,
-                child: Text('Ok'),
-              ),
-            ),
-            Expanded(
-              child: FlatButton(
-                onPressed: () {
-                  print('Ok');
-                },
-                color: Colors.green,
-                child: Text('Ok'),
-              ),
-            ),
-            Expanded(
               child: Row(
+
                 children: listScore,
               ),
             ),
