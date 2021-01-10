4cac171f48c615018809c82a340516ad8e5231df
lampart_vi_hoang_son <vihoangson@gmail.com>
2021-01-10 20:52:45 +0700

    Lesson 94: build score

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:audioplayers/audio_cache.dart';
-import 'package:http/http.dart'  as http;
+import 'package:http/http.dart' as http;
+import 'SoundAnime.dart';
+
+SoundAnime soundAnime = SoundAnime();
 // The main function is the starting point for all our flutter App.
 void main() {
   runApp(MyApp());
@@ -16,22 +18,43 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.blue,
         body: SafeArea(
-          child: FlatButton(
-            onPressed: ()async {
-              print('ok men');
-              final AudioCache player = new AudioCache();
-              player.play('audio/cello.mp3');
-              var url = 'http://ttht.oop.vn/';
-              var response = await  http.get(url);
-              print(response);
-              print(response.statusCode);
-              print(response.body);
-
-            },
-            height: 100.0,
-            color: Colors.white,
-            child: Text('cello'),
-          ),
+          child: Column(children: <Widget>[
+            FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play(soundAnime.files_url[0]);
+                },
+                child: Text('sound 1')),
+            FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play(soundAnime.files_url[1]);
+                },
+                child: Text('sound 2')),
+            FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play(soundAnime.files_url[2]);
+                },
+                child: Text('sound 3')),
+            FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play(soundAnime.files_url[3]);
+                },
+                child: Text('sound 4')),
+            FlatButton(
+                color: Colors.white,
+                onPressed: () {
+                  AudioCache player = AudioCache();
+                  player.play(soundAnime.files_url[4]);
+                },
+                child: Text('sound 5')),
+          ]),
         ),
       ),
     );
