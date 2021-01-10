e5c1c6438520bf1c30f8c33e366b67823246bf9e
lampart_vi_hoang_son <vihoangson@gmail.com>
2021-01-10 07:14:56 +0700

    first app

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:http/http.dart'  as http;
+// The main function is the starting point for all our flutter App.
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        body: SafeArea(
+          child: FlatButton(
+            onPressed: (){
+              print('ok men');
+              final AudioCache player = new AudioCache();
+              player.play('audio/cello.mp3');
+              var url = 'http://ttht.oop.vn/';
+              var response = http.get(url);
+              print(response);
+            },
+            height: 100.0,
+            color: Colors.white,
+            child: Text('cello'),
+          ),
+        ),
+      ),
+    );
+  }
+}
