6474eed1c98f7aa2b818070bf9cc6b2385333a2c
lampart_vi_hoang_son <vihoangson@gmail.com>
2021-01-24 16:53:39 +0700

    update

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -35,7 +35,16 @@ class _CityScreenState extends State<CityScreen> {
               ),
               Container(
                 padding: EdgeInsets.all(20.0),
-                child: null,
+                child: TextField(
+                  style: TextStyle(
+                    color:Colors.black,
+                  ),
+                  decoration: kInputDecoration,
+                  onChanged: (value){
+                    print(value);
+                  },
+
+                ),
               ),
               FlatButton(
                 onPressed: () {},
